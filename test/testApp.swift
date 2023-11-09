9de5128b45df4b8b485a05ba987f4116b98f6cf5
류승원 <swryu@uracle.co.kr>
2023-11-09 19:28:30 +0900

    first commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by RYUSEUNGWON on 2023/11/08.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
